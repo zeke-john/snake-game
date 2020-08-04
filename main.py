2a7eb6c92a84ba4c6ec314afb03424022e933d7e
Zeke John <zekejohn118@gmail.com>
2020-08-04 13:58:04 -0700

    added boundries

--- main.py
+++ main.py
@@ -7,8 +7,8 @@ red = ((255, 0, 0))
 black = ((0, 0, 0))
 
 pygame.init()
-display_width = 1000
-display_height = 750
+display_width = 920
+display_height = 720
 
 gameDisplay = pygame.display.set_mode((display_width, display_height))
 pygame.display.set_caption("Snake")
@@ -16,8 +16,8 @@ pygame.display.update()
 
 gameExit = False 
 
-lead_x = 375
-lead_y =  375
+lead_x = display_width/2
+lead_y = display_height/2
 
 lead_x_change_left = 0
 lead_x_change_right = 0
@@ -44,19 +44,19 @@ while not gameExit:
                 
                 
             if event.key == pygame.K_RIGHT and lead_x_change_left == 0:
-                lead_x_change_right= 6
+                lead_x_change_right= 5
                 lead_y_change_down = 0
                 lead_y_change_up = 0
                 lead_x_change_left = 0
                 
             if event.key == pygame.K_DOWN and lead_y_change_up == 0:
-                lead_y_change_down = 6
+                lead_y_change_down = 5
                 lead_x_change_left = 0
                 lead_y_change_up = 0
                 lead_x_change_right = 0
                 
             if event.key == pygame.K_UP and lead_y_change_down == 0:
-                lead_y_change_up = -6
+                lead_y_change_up = -5
                 lead_x_change_left = 0
                 lead_y_change_down = 0
                 lead_x_change_right = 0
@@ -67,8 +67,12 @@ while not gameExit:
     lead_y += lead_y_change_up
     lead_y += lead_y_change_down
     
+    if lead_x >= display_width - 16 or lead_x <= 1 or lead_y >= display_height - 16 or lead_y <= 1:
+        print("crash")
+        gameExit = True
+    
     gameDisplay.fill(green)
-    pygame.draw.rect(gameDisplay, black, [lead_x, lead_y, 10,10])
+    pygame.draw.rect(gameDisplay, black, [lead_x, lead_y, 20,20])
     pygame.display.update()
     clock.tick(60)
 
