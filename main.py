9b576a318f5cfc54a4f06624ced0a3d8e6464f7a
Zeke John <zekejohn118@gmail.com>
2020-08-05 14:52:04 -0700

    made home screen cleaner

--- main.py
+++ main.py
@@ -1,20 +1,21 @@
 import pygame
 import time
+import random
 
 pygame.init()
-grey = (170, 170, 170)
 white = (255, 255, 255)
-black = (0, 0, 0)
-red = (250, 105, 105)
-blue = (95, 95, 255)
-green = (0, 135, 0)
+black = (0,0,0)
+red = (220, 0, 0)
+blue = (0, 0, 220)
+green = (0, 170, 0)
 bright_red = (255, 0, 0)
 bright_blue = (0, 0, 255)
 
-
 large_Text = pygame.font.SysFont("C059", 115)
 small_Text = pygame.font.SysFont("C059", 20)
-pause_Text = pygame.font.SysFont("C059", 140)
+med_Text = pygame.font.SysFont("C059", 30)
+parge_Text = pygame.font.SysFont("C059", 120)
+
 
 
 display_width = 920
@@ -26,9 +27,9 @@ pygame.display.update()
 
 snake_size = 20
 
-snake_speed = 6
+snake_speed = 10
 
-fps = 60
+fps = 35
 
 clock = pygame.time.Clock()
 
@@ -46,7 +47,7 @@ def button(msg, x, y, w, h, ic, ac, action=None):
     mouse = pygame.mouse.get_pos()
     clicked = pygame.mouse.get_pressed()
 
-    if x + w > mouse[0] > x and y < mouse[1]:
+    if x + w > mouse[0] > x and y <= mouse[1]:
         pygame.draw.rect(gameDisplay, ac, (x, y, w, h))
         if clicked[0] == 1 and action != None:
             action()
@@ -75,7 +76,7 @@ def paused():
 
         gameDisplay.fill(green)
         mouse = pygame.mouse.get_pos()
-        TextSurf, TextRect = text_objects("Paused", pause_Text)
+        TextSurf, TextRect = text_objects("Paused", parge_Text)
         TextRect.center = ((display_width / 2), (display_height/2 - 30))
         gameDisplay.blit(TextSurf, TextRect)
 
@@ -104,6 +105,31 @@ def game_over():
 
         button("Replay", 230, 450, 100, 50, blue, bright_blue, game_loop)
         button("Quit", 550, 450, 100, 50, red, bright_red, game_exit)
+
+        pygame.display.update()
+        clock.tick(15)
+        
+def game_intro():
+
+    intro = True
+
+    while intro:
+        pygame.display.set_caption("Snake")
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.QUIT
+                quit()
+
+        gameDisplay.fill(green)
+        mouse = pygame.mouse.get_pos()
+        TextSurf, TextRect = text_objects("Use the arrow keys to control the snake", med_Text)
+        TextRect.center = ((display_width / 2), (display_height/2 - 40))
+        TextSur, TextRec = text_objects("Press the spacebar to pause", med_Text)
+        TextRec.center = ((display_width / 2), (display_height/2 ))
+        gameDisplay.blit(TextSurf, TextRect)
+        gameDisplay.blit(TextSur, TextRec)
+
+        button("Start Game", 400, 450, 120, 60, blue, bright_blue, game_loop)
 
         pygame.display.update()
         clock.tick(15)
@@ -170,14 +196,15 @@ def game_loop():
         lead_y += lead_y_change_up
         lead_y += lead_y_change_down
         
-        if lead_x >= display_width -15.3 or lead_x <= -3 or lead_y >= display_height - 15.3 or lead_y <= -3:
+        if lead_x >= display_width - 17 or lead_x <= -3 or lead_y >= display_height - 15 or lead_y <= -2:
             game_over()
         
         gameDisplay.fill(green)
         pygame.draw.rect(gameDisplay, black, [lead_x, lead_y, snake_size, snake_size])
         pygame.display.update()
         clock.tick(fps)
-
+        
+game_intro()
 game_loop()
 pygame.quit()
 quit()
